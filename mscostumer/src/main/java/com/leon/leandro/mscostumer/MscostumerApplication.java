856add85093209aea47e51430dcf09d71b98c02f
admin backend weconnect <back.endwc@gmail.com>
2026-01-15 21:48:59 -0500

    first commit

---
+++ mscostumer/src/main/java/com/leon/leandro/mscostumer/MscostumerApplication.java
@@ -0,0 +1,13 @@
+package com.leon.leandro.mscostumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscostumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscostumerApplication.class, args);
+	}
+
+}
---
+++ mscostumer/src/test/java/com/leon/leandro/mscostumer/MscostumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leon.leandro.mscostumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscostumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
